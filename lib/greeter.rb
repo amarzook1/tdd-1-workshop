85efbcd996ca3573363fd56e9853d515bdb31085
Ahmed <ahmed.marzook@cgi.com>
2018-10-05 09:51:30 +0000

    Greet responds to name and animal

--- lib/greeter.rb
+++ lib/greeter.rb
@@ -1,4 +1,5 @@
 class Greeter
     def greet(name, animal)
+        "bob""moo"
     end
 end
--- spec/greeter_spec.rb
+++ spec/greeter_spec.rb
@@ -1,8 +1,14 @@
 require 'greeter'
 
 describe Greeter do
-    
-    it 'responds to greet' do
-        expect(subject).to respond_to(:greet).with(2).argument
+  
+  it 'responds to greet' do
+    expect(subject).to respond_to(:greet).with(2).argument
+  end
+  
+  describe'#greet' do
+    it 'responds to ahmed' do
+        expect(subject.greet("bob", "cow")).to eq("bob" "moo")
     end
+  end
 end
